333d706d2a24ed58b5316b04f3d9454ecdeaa6e8
Kye <kye@apacmediasolutions.com>
2023-07-31 00:23:11 -0400

    clean up v number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'MedPalm',
   packages = find_packages(exclude=['examples']),
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'MedPalm - Pytorch',
   author = 'Kye Gomez',
