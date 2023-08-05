629645495dcf545aba5fa84db5245b0ed8285ffc
Kye <kye@apacmediasolutions.com>
2023-08-05 11:03:37 -0400

    short sequence

--- example.py
+++ example.py
@@ -10,10 +10,7 @@ model = MedPalm(num_tokens=1024)
 image = PIL.Image.open('image-9.png')
 
 # Create a list of different length text sequences for testing
-texts = ["This is a short sequence.",
-         "This is a longer sequence that contains more words and thus more tokens when processed by the tokenizer.",
-         "This is an even longer sequence. It contains even more words and will thus result in even more tokens when processed by the tokenizer. The purpose of this is to test how the model handles sequences of varying lengths.",
-         "This is the longest sequence used in this test. It is significantly longer than the other sequences and will result in a significantly higher number of tokens when processed by the tokenizer. The purpose of this sequence is to test the upper limits of the model's capacity to handle long sequences."]
+texts = ["This is a short sequence."]
 
 # Process each sequence and feed it into the model
 for text in texts:
