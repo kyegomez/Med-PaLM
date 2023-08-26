96e5b78edd4fd9fd7d7b6f342e843363937aa0a9
Kye <kye@apacmediasolutions.com>
2023-08-25 22:54:39 -0400

    example

--- example.py
+++ example.py
@@ -3,11 +3,10 @@ from medpalm.model import MedPalm
 
 #usage
 img = torch.randn(1, 3, 256, 256)
-text = torch.randint(0, 20000, (1, 4096))
-
-
+caption = torch.randint(0, 20000, (1, 1024))
 
 model = MedPalm()
-output = model(text, img)
+output = model(img, caption)
+print(output.shape) # (1, 1024, 20000)
 
 
